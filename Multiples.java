11530ee1b3429507469e31969fa8793c27b20f01
Mostafa Saeed <mostafa.saeedx98@gmail.com>
2023-05-14 08:17:19 +0300

    find multiples of a nmber

---
+++ Multiples.java
@@ -0,0 +1,22 @@
+package Week1_Tasks;
+
+import java.util.Scanner;
+
+public class Multiples {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        if (B % A == 0) {
+            System.out.println("Sao Multiplos");
+        }
+        else {
+            System.out.println("Nao sao Multiplos");
+        }
+        
+        
+    }
+
+}
